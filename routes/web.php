90808447c4423205aaa53522c2f748ee0682fe04
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-07-01 17:36:31 -0400

    Web changes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
   Auth::routes(['verify'=>true, 'middleware'=>'auth']);
-  Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify'); // v6.x
-
 Route::get('/boosting', function () {
     return view('layouts.order');
 });
