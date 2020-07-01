f214b20b0612fa2bc64fd5a781257e8508952df1
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-06-30 20:06:59 -0400

    Css additions

--- resources/views/layouts/order.blade.php
+++ resources/views/layouts/order.blade.php
@@ -1371,6 +1371,8 @@
                 <section class="section wow fadeIn" data-wow-delay="0.3s">
 
                       <!--Section heading-->
+                      <h1 class="font-weight-bold text-center h1 my-5 text-light">Why Choose <a class="blue">Us?</a></h1>
+
                       <!--Section description-->
                       <p class="text-center text-muted text-light mb-5 mx-auto w-responsive lead">Our team has developed a streamlined process to provide you with a stress-free experience from this page where you order your boost, to the end of your order which you can manage in your custom dashboard.</p>
 
