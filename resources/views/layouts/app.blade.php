820ee5290fa7883ecbe7c2816613510335408e3a
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-07-04 19:43:54 -0400

    Tawk.to on login and register pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,5 +150,18 @@
 
     <script src="assets/js/jquery.steps.min.js"></script>
     <script src="assets/js/bd-wizard.js"></script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/59ecd58a4854b82732ff7036/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 </html>
