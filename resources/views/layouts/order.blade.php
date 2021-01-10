c9f7cc051c99b7b79942ce0c226f5311976a7b90
TevanB <tevan.b@gmail.com>
2021-01-10 18:06:10 -0500

    discount fix

--- resources/views/layouts/order.blade.php
+++ resources/views/layouts/order.blade.php
@@ -1670,7 +1670,7 @@
             "Content-Type": "application/json"
           },
           body: JSON.stringify({
-              price: orderPrice,
+              price: reqPrice,
               description: orderPPString,
               id: orderIdUnique,
             })
