9e2d917fda0e5e8520129cff6454a5d48bd35362
TevanB <tevan.b@gmail.com>
2021-02-15 19:28:53 -0500

    fixing stripepayout finally

--- app/Http/Controllers/StripeRelated/StripePayout.php
+++ app/Http/Controllers/StripeRelated/StripePayout.php
@@ -7,6 +7,32 @@ require_once('stripe-php/init.php');
 class StripePayout
 {
 
+
+    public static function visitPayoutDash($debug=false){
+        \Stripe\Stripe::setApiKey('sk_test_51I4xTLCBjNIgSDtBbDv1cPl9TO0vag9FwJp5GQ0I1pFlo8n7a16uuYVmN6RUfDBvu6jRwrHcNFZ8qAdQZ4blh4HR000HgbL963');
+        try{
+            $json_str = file_get_contents('php://input');
+            $json_obj = json_decode($json_str);
+            $stripe = new \Stripe\StripeClient(
+                'sk_test_51I4xTLCBjNIgSDtBbDv1cPl9TO0vag9FwJp5GQ0I1pFlo8n7a16uuYVmN6RUfDBvu6jRwrHcNFZ8qAdQZ4blh4HR000HgbL963'
+              );
+            $accList = $stripe->accounts->all(['limit' => 3]);
+            $accId = "";
+            for ($x = 0; $x < count($accList->data); $x++) {
+                if($json_obj->email == $accList->data[$x]){
+                    $accId = $accList->data[$x]->id;
+                }
+            }
+            if($accId != ""){
+                echo(createAccLink($accId));
+            }else{
+                echo(createAcc());
+            }
+        }catch(Error $e){
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+    }
     public static function createAcc($debug=false){
         \Stripe\Stripe::setApiKey('sk_test_51I4xTLCBjNIgSDtBbDv1cPl9TO0vag9FwJp5GQ0I1pFlo8n7a16uuYVmN6RUfDBvu6jRwrHcNFZ8qAdQZ4blh4HR000HgbL963');
         try{
@@ -47,31 +73,6 @@ class StripePayout
             ]);
             echo($account_links);
 
-        }catch(Error $e){
-            http_response_code(500);
-            echo json_encode(['error' => $e->getMessage()]);
-        }
-    }
-    public static function visitPayoutDash($debug=false){
-        \Stripe\Stripe::setApiKey('sk_test_51I4xTLCBjNIgSDtBbDv1cPl9TO0vag9FwJp5GQ0I1pFlo8n7a16uuYVmN6RUfDBvu6jRwrHcNFZ8qAdQZ4blh4HR000HgbL963');
-        try{
-            $json_str = file_get_contents('php://input');
-            $json_obj = json_decode($json_str);
-            $stripe = new \Stripe\StripeClient(
-                'sk_test_51I4xTLCBjNIgSDtBbDv1cPl9TO0vag9FwJp5GQ0I1pFlo8n7a16uuYVmN6RUfDBvu6jRwrHcNFZ8qAdQZ4blh4HR000HgbL963'
-              );
-            $accList = $stripe->accounts->all(['limit' => 3]);
-            $accId = "";
-            for ($x = 0; $x < count($accList->data); $x++) {
-                if($json_obj->email == $accList->data[$x]){
-                    $accId = $accList->data[$x]->id;
-                }
-            }
-            if($accId != ""){
-                echo(createAccLink($accId));
-            }else{
-                echo(createAcc());
-            }
         }catch(Error $e){
             http_response_code(500);
             echo json_encode(['error' => $e->getMessage()]);
